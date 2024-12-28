1729b4265dc15121c9412d3ab9286c1bd05e56e9
vito <working.space.js@gmail.com>
2024-12-28 14:56:32 +0100

    add yaml file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import glob from "glob";
 import injectHTML from "vite-plugin-html-inject";
 import FullReload from "vite-plugin-full-reload";
+import envCompatible from "vite-plugin-env-compatible";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -15,6 +16,7 @@ export default defineConfig(({ command }) => {
     build: {
       base: "/portfolio/",
       sourcemap: true,
+
       rollupOptions: {
         input: glob.sync("./src/*.html"),
         output: {
@@ -28,7 +30,7 @@ export default defineConfig(({ command }) => {
       },
       outDir: "../dist",
     },
-    plugins: [injectHTML(), FullReload(["./src/**/**.html"])],
+    plugins: [injectHTML(), FullReload(["./src/**/**.html"]), envCompatible()],
     optimizeDeps: {
       entries: [],
     },
